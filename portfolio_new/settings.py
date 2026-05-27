4bdcc4b7119c29d3768d60e907144fdff3d84181
C VISHNU DEEPAK <22ng1a5410@usharama.in>
2026-05-27 17:37:00 +0530

    Update settings.py

--- portfolio_new/settings.py
+++ portfolio_new/settings.py
@@ -36,7 +36,9 @@ ROOT_URLCONF = "portfolio_new.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(str(BASE_DIR), "templates")],
+        "DIRS": [
+            os.path.join(str(BASE_DIR), "templates"),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
